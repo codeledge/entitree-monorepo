a25e8bea7e2de5eccb180bf646db0d99e0eec394
mshd <mshd94@gmail.com>
2022-03-18 15:21:07 +0800

    mv

--- packages/helpers/tests/youtube-helper.test.ts
+++
@@ -1,6 +0,0 @@
-import { durationToSeconds } from "../src/";
-
-test("Youtube duration to seconds", () => {
-  expect(durationToSeconds("PT27M42S")).toBe(1662);
-  expect(durationToSeconds("PT1H5M43S")).toBe(3943);
-});
