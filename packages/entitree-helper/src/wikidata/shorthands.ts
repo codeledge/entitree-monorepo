8d1cb0c205eec2a2ecb39463ff17d55478dadf18
mshd <mshd94@gmail.com>
2022-04-30 20:33:37 +0800

    add diff prefix for QIDS

--- packages/entitree-helper/src/wikidata/shorthands.ts
+++ packages/entitree-helper/src/wikidata/shorthands.ts
@@ -1,30 +1,30 @@
 // items
-export const WD_HUMAN = "Q5";
-export const WD_FICTIONAL_HUMAN = "Q15632617";
-export const WD_HUMAN_FETUS = "Q26513";
-export const WD_ORGANIZATION = "Q43229";
-export const WD_BUSINESS = "Q4830453";
-export const WD_HUMAN_SETTLEMENT = "Q486972";
-export const WD_HUMAN_MALE = "Q6581097";
-export const WD_ANIMAL_MALE = "Q44148";
-export const WD_HUMAN_FEMALE = "Q6581072";
-export const WD_ANIMAL_FEMALE = "Q43445";
-export const WD_ENGLISH = "Q1860";
+export const WDQ_HUMAN = "Q5";
+export const WDQ_FICTIONAL_HUMAN = "Q15632617";
+export const WDQ_HUMAN_FETUS = "Q26513";
+export const WDQ_ORGANIZATION = "Q43229";
+export const WDQ_BUSINESS = "Q4830453";
+export const WDQ_HUMAN_SETTLEMENT = "Q486972";
+export const WDQ_HUMAN_MALE = "Q6581097";
+export const WDQ_ANIMAL_MALE = "Q44148";
+export const WDQ_HUMAN_FEMALE = "Q6581072";
+export const WDQ_ANIMAL_FEMALE = "Q43445";
+export const WDQ_ENGLISH = "Q1860";
 
 // qualifiers
-export const WD_CIRCA = "Q5727902";
-export const WD_PRESUMABLY = "Q18122778";
+export const WDQ_CIRCA = "Q5727902";
+export const WDQ_PRESUMABLY = "Q18122778";
 
 //podcast
-export const WD_EXPLICIT_EPISODE = "Q109501804";
-export const WD_PODCAST = "Q24634210";
-export const WD_PODCAST_EPISODE = "Q61855877";
-export const WD_SECOND = "Q11574";
-export const WD_INFERRED_FROM_YOUTUBE_DESCRIPTION = "Q110039923";
-export const WD_INFERRED_FROM_PODCAST_DESCRIPTION = "Q110068003";
-export const WD_INFERRED_FROM_TITLE = "Q69652283";
-export const WD_EPISODE_TYPE_MATCH = {
-  full: WD_PODCAST_EPISODE,
+export const WDQ_EXPLICIT_EPISODE = "Q109501804";
+export const WDQ_PODCAST = "Q24634210";
+export const WDQ_PODCAST_EPISODE = "Q61855877";
+export const WDQ_SECOND = "Q11574";
+export const WDQ_INFERRED_FROM_YOUTUBE_DESCRIPTION = "Q110039923";
+export const WDQ_INFERRED_FROM_PODCAST_DESCRIPTION = "Q110068003";
+export const WDQ_INFERRED_FROM_TITLE = "Q69652283";
+export const WDQ_EPISODE_TYPE_MATCH = {
+  full: WDQ_PODCAST_EPISODE,
   trailer: "Q106677532",
   bonus: "Q110730867",
 };
