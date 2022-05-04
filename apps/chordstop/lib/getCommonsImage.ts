4eec90be5c7f3b00df56c43efe9bfe62797fc53c
mshd <mshd94@gmail.com>
2022-05-04 18:13:03 +0800

    console

--- apps/chordstop/lib/getCommonsImage.ts
+++ apps/chordstop/lib/getCommonsImage.ts
@@ -1,9 +1,6 @@
 export function getCommonsUrlByFile(filename: string, size: number) {
-  console.log(filename);
-
   if (filename.includes("FilePath/")) {
     filename = filename.split("FilePath/")[1];
   }
-  console.log(filename);
   return `https://commons.wikimedia.org/wiki/Special:FilePath/${filename}?width=${size}px`;
 }
