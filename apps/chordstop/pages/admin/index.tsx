458d31368e7038a5ac8f4d0ecab7cd6fbdef1b17
mshd <mshd94@gmail.com>
2022-05-04 20:03:48 +0800

    stop errr for admin JSX

--- apps/chordstop/pages/admin/index.tsx
+++ apps/chordstop/pages/admin/index.tsx
@@ -1,11 +1,11 @@
+//@ts-nocheck
 import dynamic from "next/dynamic";
 
 const ReactAdmin = dynamic(() => import("../../components/ReactAdmin"), {
   ssr: false,
 });
-
-const HomePage = () => {
+const AdminPage = () => {
   return <ReactAdmin />;
 };
 
-export default HomePage;
+export default AdminPage;
