a609465f35385cb82c14310918d34186b7b81419
mshd <mshd94@gmail.com>
2022-05-04 17:55:36 +0800

    recover

---
+++ apps/chordstop/components/Fields/ImageAllField.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { getCommonsUrlByFile } from "../../lib/getCommonsImage";
+import { useRecordContext } from "react-admin";
+
+export const ImageCustomField = (props: {
+  label?: string;
+  title?: string;
+  record?: {};
+  source: string;
+}) => {
+  const { source, title } = props;
+  const record = useRecordContext(props);
+
+  return (
+    <div>
+      <img
+        alt=""
+        src={getCommonsUrlByFile(record[source], 200)}
+        title={title}
+        width={200}
+        style={{ backgroundColor: "grey" }}
+      />
+    </div>
+  );
+};
