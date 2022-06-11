b030d387b2a4e6df22451ce77bae09387839dad5
mshd <mshd94@gmail.com>
2022-06-11 17:03:16 +0800

    disable stats

--- apps/entitree-images/lib/statsUpdater.ts
+++ apps/entitree-images/lib/statsUpdater.ts
@@ -2,24 +2,24 @@ import { prismaClient, Metric } from "../prisma/prismaClient";
 
 export async function updateMetric(metric: Metric) {
   const month = new Date().toISOString().slice(0, 7);
-  await prismaClient.stat.upsert({
-    where: {
-      metric_month: {
-        metric,
-        month,
-      },
-    },
-    create: {
-      count: 1,
-      metric,
-      month,
-    },
-    update: {
-      count: {
-        increment: 1,
-      },
-    },
-  });
+  // await prismaClient.stat.upsert({
+  //   where: {
+  //     metric_month: {
+  //       metric,
+  //       month,
+  //     },
+  //   },
+  //   create: {
+  //     count: 1,
+  //     metric,
+  //     month,
+  //   },
+  //   update: {
+  //     count: {
+  //       increment: 1,
+  //     },
+  //   },
+  // });
 }
 
 export { Metric } from "../prisma/prismaClient";
