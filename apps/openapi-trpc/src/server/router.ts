bacb6dd1075cb3e97d40575738d3cd63a420e61b
mshd <mshd94@gmail.com>
2022-07-10 21:10:00 +0800

    more routers (#14)

--- apps/openapi-trpc/src/server/router.ts
+++ apps/openapi-trpc/src/server/router.ts
@@ -1,9 +1,11 @@
-import { wikidataRouter } from "./wikidataRouter";
 import { createRouter } from "./createRouter";
-import { documentRouter } from "./documentRouter";
+import { applePodcastRouter } from "./services/applePodcastRouter";
+import { documentRouter } from "./services/documentRouter";
+import { wikidataRouter } from "./services/wikidataRouter";
 
 export const appRouter = createRouter()
   .merge(wikidataRouter)
-  .merge(documentRouter);
+  .merge(documentRouter)
+  .merge(applePodcastRouter);
 
 export type AppRouter = typeof appRouter;
