da4571d8865dfb11576fbdf0772bdfbb28af2fce
mshd <mshd94@gmail.com>
2022-12-10 13:05:44 +0800

    tempfix for typeerror annotations

--- apps/openapi-trpc/src/utils/trpc.ts
+++ apps/openapi-trpc/src/utils/trpc.ts
@@ -2,7 +2,7 @@ import { createTRPCNext } from "@trpc/next";
 import { httpBatchLink } from "@trpc/client";
 import { AppRouter } from "../server/router";
 
-export const trpc = createTRPCNext<AppRouter>({
+export const trpc: any = createTRPCNext<AppRouter>({
   config() {
     return {
       links: [
