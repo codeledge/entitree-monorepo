7a5e1b9e0ad4d00bbf6c3c60ac38316c94ac5db1
mshd <mshd94@gmail.com>
2022-12-10 13:10:08 +0800

    Commit

--- packages/entitree-helper/tests/getWikipediaArticle.test.ts
+++ packages/entitree-helper/tests/getWikipediaArticle.test.ts
@@ -1,13 +1,13 @@
 import { getWikipediaArticle, getWikipediaDescription } from "../src";
 
-test("WikipediaARticle", async () => {
+test("Wikipedia Article", async () => {
   const wikipedia = await getWikipediaArticle("Anthoni_Salim", "en");
   expect(wikipedia.extract).toContain("Indonesian businessman");
   expect(wikipedia.description).toBe("Chinese-Indonesian businessman");
 });
-// test("it should give the correct name", async () => {
-//   const res = await getWikipediaDescription("Elvis_Presley");
-//   expect(res).toBe(
-//     `Elvis Aaron Presley was an American singer and actor. Dubbed the \"King of Rock and Roll\", he is regarded as one of the most significant cultural icons of the 20th century. His energized interpretations of songs and sexually provocative performance style, combined with a singularly potent mix of influences across color lines during a transformative era in race relations, led him to both great success and initial controversy.`
-//   );
-// });
+
+test("Wikipedia Article with slash in name", async () => {
+  const wikipedia = await getWikipediaArticle("Sex/Life", "en");
+  expect(wikipedia.extract).toContain("television");
+  expect(wikipedia.description).toContain("television series");
+});
