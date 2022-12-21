cf8dd7076634804d527bdafb8ef42264090649a4
mshd <mshd94@gmail.com>
2022-12-22 00:21:50 +0800

    new extension

--- apps/entitree-images/prisma/prismaClient.ts
+++ apps/entitree-images/prisma/prismaClient.ts
@@ -1,5 +1,16 @@
 import { PrismaClient } from "./generated/client";
 
-export const prismaClient = new PrismaClient();
+export const prismaClient = new PrismaClient().$extends({
+  result: {
+    image: {
+      imageUrl: {
+        needs: { id: true },
+        compute: ({ id }) => {
+          return `https://storage.googleapis.com/entitree-images/${id}.jpg`;
+        },
+      },
+    },
+  },
+});
 
 export { Metric, type Image, ActionStatus, Prisma } from "./generated/client";
