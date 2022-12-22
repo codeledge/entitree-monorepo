33540dba4ba5ac962d22beb43624d6940c0086dd
mshd <mshd94@gmail.com>
2022-12-22 17:52:10 +0800

    add google analytics

--- apps/wikidata-table/pages/_app.tsx
+++ apps/wikidata-table/pages/_app.tsx
@@ -1,5 +1,10 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(process.env.NEXT_PUBLIC_GA_TRACKING_CODE || "placeholder", {
+  testMode: !process.env.NEXT_PUBLIC_GA_TRACKING_CODE,
+});
 
 function ReactAdminApp({ Component, pageProps }: AppProps) {
   return (
