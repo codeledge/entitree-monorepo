1013edc84dee1d7d3b151bdb235ee063deb23600
mshd <mshd94@gmail.com>
2023-05-03 12:50:30 +0800

    next auth simplier

--- apps/entitree-images/pages/api/auth/[...nextauth].ts
+++ apps/entitree-images/pages/api/auth/[...nextauth].ts
@@ -3,28 +3,26 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prismaClient } from "../../../prisma/prismaClient";
 
-export default async function auth(req, res) {
-  return await NextAuth(req, res, {
-    //debug: true,
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      }),
-    ],
-    secret: process.env.NEXTAUTH_SECRET_KEY,
-    callbacks: {
-      async session({ session, token, user }) {
-        const userDoc = await prismaClient.user.findFirst({
-          where: {
-            id: parseInt(user.id),
-          },
-        });
-        session.userId = parseInt(user.id);
-        session.role = userDoc?.role;
-        return session;
-      },
+export default NextAuth({
+  //debug: true,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET_KEY,
+  callbacks: {
+    async session({ session, token, user }) {
+      const userDoc = await prismaClient.user.findFirst({
+        where: {
+          id: parseInt(user.id),
+        },
+      });
+      session.userId = parseInt(user.id);
+      session.role = userDoc?.role;
+      return session;
     },
-    adapter: PrismaAdapter(prismaClient),
-  });
-}
+  },
+  adapter: PrismaAdapter(prismaClient),
+});
